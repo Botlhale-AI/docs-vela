3c9645be34651e81585511398fc06c42325fb9d0
madlalakhaya25 <madlalakhaya@gmail.com>
2025-08-21 10:52:52 +0200

    chore: remove node_modules and other build files from source control
    - Remove all node_modules files from Git tracking
    - Remove package-lock.json and yarn.lock from tracking
    - Remove build output files
    - Update .gitignore with proper Docusaurus and Node.js patterns

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/athi/Desktop/docs-vela/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/athi/Desktop/docs-vela/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/athi/Desktop/docs-vela/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/athi/Desktop/docs-vela/src/css/custom.module.css"),
-];
