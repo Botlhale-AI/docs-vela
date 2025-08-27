8da98a24c0f81dd8b9084e281ac052b99a016fca
madlalakhaya25 <madlalakhaya@gmail.com>
2025-08-27 13:04:49 +0200

    Deploy website - based on a7eea26b8d0afa7fa554d50ac44b4a09de071618

--- assets/js/a6cdc34d.76e76381.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkvela_documentation=self.webpackChunkvela_documentation||[]).push([[1845],{4207:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/docs-vela/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
