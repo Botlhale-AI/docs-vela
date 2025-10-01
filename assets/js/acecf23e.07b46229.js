7f43290f8ac3626c9caf3ab9c21104e3d66c5f1c
Khaya Madlala <madlalakhaya@gmail.com>
2025-10-01 12:53:30 +0200

    Deploy website - based on e47ea566cf3ee79940c81abf3a01e85e360593ce

--- assets/js/acecf23e.07b46229.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkvela_documentation=self.webpackChunkvela_documentation||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/docs-vela/blog","blogTitle":"Blog","authorsListPath":"/docs-vela/blog/authors"}')}}]);
